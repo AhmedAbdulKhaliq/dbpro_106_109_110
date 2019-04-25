1b893634a05a9ce54986a455f172f7f0023a199e
AhmedAbdulKhaliq <ahmed106ak@gmail.com>
2019-04-25 08:39:46 +0500

    .

---
+++ IMS/IMS/Models/StudentModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMS.Models
+{
+    public class StudentModels
+    {
+        public class StudentRegisterModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Contact { get; set; }
+            public string Cnic { get; set; }
+            public string City { get; set; }
+            public System.DateTime DOB { get; set; }
+            public System.DateTime RegistrationDate { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
